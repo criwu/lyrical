8ab4df67923a4d59fe26389b280a1a84bced7b45
City <city@nanzc.com>
2021-11-02 16:42:25 +0800

    feat: Skeleton

---
+++ packages/react/src/components/Skeleton/Skeleton.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import './skeleton.styl'
+
+interface IProps {
+  active?: boolean
+  block?: boolean
+  height?: number
+  style?: React.CSSProperties
+  backgroundColor?: string
+  activeColor?: string
+}
+
+const Skeleton = (props: IProps) => {
+  const {
+    active = false,
+    block = false,
+    height = 32,
+    backgroundColor = 'rgba(190,190,190,.2)',
+    activeColor = 'rgba(129,129,129,.24)',
+    style = {}
+  } = props
+
+  return (
+    <div
+      className={`skeleton${active ? ' active' : ''}${block ? ' block' : ''}`}
+      style={{
+        height,
+        backgroundImage: active
+          ? `linear-gradient(90deg, ${backgroundColor} 25%, ${activeColor} 37%, ${backgroundColor} 63%)`
+          : backgroundColor,
+        ...style
+      }}
+    />
+  )
+}
+
+export default Skeleton
